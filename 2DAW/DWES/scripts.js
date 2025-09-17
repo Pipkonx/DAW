62302a0bae38aa9626f39c30a71d6bd80c6e0b9e
RafaCrdr <corderorafa0@gmail.com>
2025-09-17 16:52:45 +0200

    Hazaña: Incorporar el juego de adivinación de números y el verificador de palíndromos.
    Implementar el ejercicio 3 con lógica de adivinación de números y el ejercicio 4 con detección de palíndromos.

--- 2DAW/DWES/scripts.js
+++ 2DAW/DWES/scripts.js
@@ -1,29 +1,51 @@
 function ejercicio1() {
-    let frase = prompt("Escribe una frase:");
-    let letra = prompt("Elige una letra");
-    frase = frase.split('')
-    let contador = 0;
+  let frase = prompt("Escribe una frase:");
+  let letra = prompt("Elige una letra");
+  frase = frase.split("");
+  let contador = 0;
 
-    for (let i = 0; i < frase.length; i++) {
-        if (frase[i] === letra) {
-            contador++;
-        }
+  for (let i = 0; i < frase.length; i++) {
+    if (frase[i] === letra) {
+      contador++;
     }
-    alert("la letra " + letra + "de la frase aparece " + contador + " veces");
+  }
+  alert("la letra " + letra + "de la frase aparece " + contador + " veces");
 }
 
+//!Corregir
+// Solicitaamos una frase formada por letras y números y tiene que decirnos cuántas letras y cuantos números tiene y la suma de éstos
 function ejercicio2() {
-    let frase = prompt("Introduce una frase formada por letras y numeros:");
-    let frase1 = "", numero=0, letra="";
-    
-
+  let frase = prompt("Introduce una frase formada por letras y numeros:");
+  let numero = 0;
+ 
 }
 
+// el mathfloor redonde el numero
 function ejercicio3() {
+  let numeroAleatorio = Math.floor(Math.random() * 100) + 1;
+  //console.log(numeroAleatorio);
+  let intento;
 
+  while (intento != numeroAleatorio) {
+    intento = prompt("Adivina el número (entre 1 y 100):");
+    if (intento < numeroAleatorio) {
+      alert("El número es mayor");
+    } else if (intento > numeroAleatorio) {
+      alert("El número es menor");
+    }
+  }
+  alert("¡Has adivinado el número!");
 }
 
+//!Corregir
+//  sabr si una palabra es palindroma
 function ejercicio4() {
+  let palabra = prompt("Introduce una palabra:");
+  let esPalindroma = true;
 
+  if (esPalindroma) {
+    alert("La palabra es palíndroma");
+  } else {
+    alert("La palabra no es palíndroma");
+  }
 }
-
