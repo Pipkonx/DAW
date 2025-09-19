2aa9ed56482e2168ffaf267dca9fc1fd9e42a966
Pipkon <pipkon.proyectos@gmail.com>
2025-09-19 14:22:16 +0200

    ejercicio basico de script python de suma de dos numeros

---
+++ 2DAW/Python/ejercicioBasico.py
@@ -0,0 +1,7 @@
+print("Introduce un numero")
+numero = input()
+print("Introduce el segundo numero")
+numeroDos = input()
+resultado = int(numero)  + int(numeroDos)
+
+print(f"El resultado de {numero} mas {numeroDos} es {resultado}")
