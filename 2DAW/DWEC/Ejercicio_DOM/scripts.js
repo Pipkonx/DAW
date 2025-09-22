6698084fc794e3f075bdde6cd82fb6cac501461e
Pipkon <pipkon.proyectos@gmail.com>
2025-09-22 13:29:15 +0200

    Característica: Añadir ejercicios de DOM con estilo y funcionalidad
    - Añadir favicono y hoja de estilo para mejorar la presentación visual
    - Implementar tres ejercicios de manipulación de DOM con su correspondiente interfaz de usuario
    - Mejorar la estructura HTML con secciones semánticas y pie de página
    - Añadir estilos CSS para componentes interactivos y adaptables

--- 2DAW/DWEC/Ejercicio_DOM/scripts.js
+++ 2DAW/DWEC/Ejercicio_DOM/scripts.js
@@ -4,5 +4,17 @@ function muestraTitulo() {
     titulo = document.getElementById("titulo");
     // alert(titulo.innerHTML)
     // console.log(titulo.innerHTML)
-    titulo.innerHTML = "Hola DOM";
+    titulo.innerHTML = "Hola Mundo!";
+}
+
+function contador() {
+    cuenta = document.getElementById("cuenta");
+    cuenta.innerHTML = parseInt(cuenta.innerHTML) + 1;
+}
+
+function parrafos(){
+    parrafos = document.getElementsByTagName("p");
+    parrafo = prompt("Parrafo a cambiar [0-2]: ");
+    parrafos[parrafo].innerHTML = "PARRAFO CAMBIADO";
+
 }
