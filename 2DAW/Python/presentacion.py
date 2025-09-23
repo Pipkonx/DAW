652802994a85a19e97aec9e01d6fecd280195f92
Pipkon <pipkon.proyectos@gmail.com>
2025-09-23 10:59:23 +0200

    python basico

---
+++ 2DAW/Python/presentacion.py
@@ -0,0 +1,11 @@
+nombre = input("Como te llamas: ")
+edad = input("Cual es tu edad: ")
+
+print(f"Hola, {nombre}, tienes {edad} años")
+
+suma = int(edad) + 5
+
+print(f"Dentro de 5 años tendrás: {int(edad) + 5} años de edad")
+print(f"Dentro de 5 años tendrás: {suma} años de edad")
+print(f"Dentro de 5 años tendrás:", suma,  "años de edad")
+print(f"Dentro de 5 años tendrás:", int(edad) + 5,  "años de edad")
