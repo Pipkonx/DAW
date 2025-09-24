bd767e55bcd2bd881dc60e4a654e2ba9b9b20c1a
Pipkon <pipkon.proyectos@gmail.com>
2025-09-24 08:50:05 +0200

    Característica: Añadir funcionalidad y estilos para la suma de tablas
    - Implementar la función sumaCelda para calcular la suma de celdas de tablas
    - Añadir estilos CSS para tablas y extensiones
    - Actualizar el HTML con la nueva estructura de tablas y secciones de ejercicios

--- 2DAW/DWEC/Ejercicio_DOM/scripts.js
+++ 2DAW/DWEC/Ejercicio_DOM/scripts.js
@@ -12,9 +12,32 @@ function contador() {
     cuenta.innerHTML = parseInt(cuenta.innerHTML) + 1;
 }
 
-function parrafos(){
+function parrafos() {
     parrafos = document.getElementsByTagName("p");
-    parrafo = prompt("Parrafo a cambiar [0-2]: ");
+    parrafo = prompt("Parrafo a cambiar [1-3]: ") - 1;
     parrafos[parrafo].innerHTML = "PARRAFO CAMBIADO";
-
 }
+
+function sumaCelda() {
+    let suma = 0;
+    celdas = document.getElementsByTagName("td");
+
+    for (let i = 0; i < celdas.length; i++) {
+        suma = suma + parseInt(celdas[i].innerHTML);
+    }
+    let sumaDerecha = 0;
+    celdasDerecha = document.getElementsByClassName("derecha");
+
+    for (let i = 0; i < celdasDerecha.length; i++) {
+        sumaDerecha = sumaDerecha + parseInt(celdasDerecha[i].innerHTML);
+    }
+    document.getElementById("suma").innerHTML = "SUMA : " + suma + "<br>Suma derecha : " + sumaDerecha;
+}
+
+
+
+
+
+
+
+
