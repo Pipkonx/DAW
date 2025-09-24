07e5d3be26d78d9e92a72707aa11ebc4cf6b881d
Pipkon <pipkon.proyectos@gmail.com>
2025-09-24 14:20:11 +0200

    Característica: añadir estilo básico y mejorar la disposición de los ejercicios
    - Añadir un nuevo archivo style.css con estilo básico para el cuerpo
    - Mover elementos HTML de las sentencias de impresión de PHP a HTML directo
    - Añadir líneas horizontales entre los ejercicios para una mejor separación visual
    - Corregir el formato del código en la condición del bucle While

--- 2DAW/DWES/htdocs/EjerciciosBasicos/T2/index.php
+++ 2DAW/DWES/htdocs/EjerciciosBasicos/T2/index.php
@@ -5,16 +5,22 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
+    <h1>Tema 2</h1>
+    <hr>
+    <h3>Ejercicio 1</h3><br>
+    <p>Tabla de multiplicar</p>
+
     <?php
-    print("<h1>Tema 2</h1><br><h3>Ejercicio 1</h3><br><p>Tabla de multiplicar</p>");
     for ($i = 0; $i < 11; $i++) {
         print($i . " x " . 5 . " = " . 5 * $i . "<br>");
     }
 
-    print("<br><h3>Ejercicio 2</h3><br><p>Tabla de multiplicar del 1 al 10 con for anidados</p>");
+
+    print("<hr><h3>Ejercicio 2</h3><br><p>Tabla de multiplicar del 1 al 10 con for anidados</p>");
     for ($a = 0; $a < 11; $a++) {
         print("<br><br>");
         for ($i = 0; $i < 11; $i++) {
@@ -22,13 +28,13 @@
         }
     }
 
-    print("<br><h3>Ejercicio 3</h3><br><p>Tabla de numero aleatorio con rand()</p>");
+    print("<hr><h3>Ejercicio 3</h3><br><p>Tabla de numero aleatorio con rand()</p>");
     $a = rand(1, 10);
     for ($i = 0; $i < 11; $i++) {
         print($i . " x " . $a . " = " . $a * $i . "<br>");
     }
 
-    print("<br><h3>Ejercicio 4</h3><br><p>Sacamos un numero aleatorio con rand y luego decimos que numero es con un switch</p><br>");
+    print("<hr><h3>Ejercicio 4</h3><br><p>Sacamos un numero aleatorio con rand y luego decimos que numero es con un switch</p><br>");
     $a = rand(1, 10);
     switch ($a) {
         case 1:
@@ -62,26 +68,25 @@
             print("El numero aleatorio es diez");
             break;
     }
-    print("<br><br><h3>Ejercicio 5</h3><br><p>Mostramos todos los numeros entre el 1 y el 1000 que son multiplos de 3, 4</p><br>");
+    print("<hr><h3>Ejercicio 5</h3><br><p>Mostramos todos los numeros entre el 1 y el 1000 que son multiplos de 3, 4</p><br>");
     for ($i = 0; $i < 1001; $i++) {
         if ($i % 3 == 0 && $i % 4 == 0) {
             print($i . " <br>");
         }
     }
 
-
-    print("<br><br><h3>Ejercicio 6</h3><br><p>Mostramos todos los numeros que son multiplos de 3, 4 y 7</p><br>");
+    print("<hr><h3>Ejercicio 6</h3><br><p>Mostramos todos los numeros que son multiplos de 3, 4 y 7</p><br>");
     for ($i = 0; $i < 1001; $i++) {
         if ($i % 3 == 0 && $i % 4 == 0 && $i % 7 == 0) {
             print($i . " <br>");
         }
     }
 
-    print("<br><br><h3>Ejercicio 7</h3><br><p></p><br>");
+    print("<hr><h3>Ejercicio 7</h3><br><p></p><br>");
     do {
         $i = rand(0, 1000);
         print($i . "<br>");
-    } while($i == 15);
+    } while ($i == 15);
     ?>
 
 </body>
