7f34571e84a2e4f33cea474c0038f22e688baa46
Pipkon <pipkon.proyectos@gmail.com>
2025-09-26 14:18:23 +0200

    correción de isupper

--- 2DAW/Python/ActEvaluables/06_login.py
+++ 2DAW/Python/ActEvaluables/06_login.py
@@ -11,7 +11,7 @@ def registrarUsu():
         if len(passwd) < 8:
             print("Contraseña insegura ❌. Debe tener al menos 8 caracteres.")
             continue
-        elif passwd.isupper():
+        elif passwd.isupper() is False:
             print("Contraseña insegura ❌. Debe tener al menos una mayúscula.")
             continue
         elif not any(char.isdigit() for char in passwd):
