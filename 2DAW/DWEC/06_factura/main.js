9b3e18d701445c85f1f7fc4a99b389ae44ab7486
RafaCrdr <corderorafa0@gmail.com>
2025-10-06 18:03:00 +0200

    corrección del ejercicio de tablas

--- 2DAW/DWEC/06_factura/main.js
+++ 2DAW/DWEC/06_factura/main.js
@@ -8,97 +8,65 @@ function calcularTotal() {
 }
 
 function agregarProducto() {
-    let codigo = document.querySelector("#Codigo").value;
-    let descripcion = document.querySelector("#Descripcion").value;
-    let cantidad = document.querySelector("#Cantidad").value;
-    let precio = document.querySelector("#Precio").value;
+    let codigo = document.querySelector("#codigo").value;
+    let descripcion = document.querySelector("#descripcion").value;
+    let cantidad = document.querySelector("#cantidad").value;
+    let precio = document.querySelector("#precio").value;
     let total = (cantidad * precio).toFixed(2);
-
-    let boton = document.createElement("button");
-    let botonModificar = document.createElement("button");
 
     let NuevaFila = [codigo, descripcion, cantidad, precio, total];
 
     let fila = document.createElement("tr");
     document.getElementById("tabla").appendChild(fila);
 
+    // celdas con los datos
     for (let i = 0; i < NuevaFila.length; i++) {
         let columna = document.createElement("td");
-        // if (i === 0) {
-        //     columna.id = "Codigo" + i;
-        // }
-        // if (i === 1) {
-        //     columna.id = "Descripcion"+ i;
-        // }
-        // if (i === 2) {
-        //     columna.id = "Cantidad" + i;
-        // }
-        // if (i === 3) {
-        //     columna.id = "Precio" + i;
-        // }
         if (i === 4) {
             columna.id = "subtotal";
         }
-
         columna.innerHTML = NuevaFila[i];
         fila.appendChild(columna);
-
-
-        // boton de eliminar
-        boton.innerHTML = '❌';
-        boton.addEventListener("click", function () {
-            // let i = x.parentNode.parentNode.rowIndex;
-            // document.getElementById("tabla").deleteRow(i);
-            fila.remove();
-        });
-
-        fila.appendChild(boton);
-
-        // boton de modificar
-        botonModificar.innerHTML = '🔑';
-        botonModificar.addEventListener("click", function () {
-            let celdas = fila.querySelectorAll("td");
-            let CodigoD = document.querySelectorAll("#CodigoD");
-            let DescripcionD = document.querySelectorAll("#DescripcionD");
-            let CantidaD = document.querySelectorAll("#CantidadD");
-            let PrecioD = document.querySelectorAll("#PrecioD");
-
-            // cogems los valores
-            CodigoD[0].value = celdas[0].textContent;
-            DescripcionD[0].value = celdas[1].innerHTML;
-            CantidaD[0].value = celdas[2].innerHTML;
-            PrecioD[0].value = celdas[3].innerHTML;
-
-            NuevaFila[i].innerHTML = celdas[i].innerHTML;
-            // console.log(celdas[i]);
-        });
-        fila.appendChild(botonModificar);
     }
 
-    calcularTotal();
-}
-
-function modificarProducto(x) {
-    // let fila = x.parentNode.parentNode;
-    let valores = x.parentNode.parentNode.children;
-
-    document.getElementById("CodigoD").value = valores[0].innerHTML;
-    document.getElementById("DescripcionD").value = valores[1].innerHTML;
-    document.getElementById("CantidadD").value = valores[2].innerHTML;
-    document.getElementById("PrecioD").value = valores[3].innerHTML;
-
-    document.getElementById("modificar").onclick = function () {
-        valores[0].innerHTML = document.getElementById("CodigoD").value;
-        valores[1].innerHTML = document.getElementById("DescripcionD").value;
-        valores[2].innerHTML = document.getElementById("CantidadD").value;
-        valores[3].innerHTML = document.getElementById("PrecioD").value;
-        console.log(valores[4].innerHTML);
-
-        let cantidad = parseFloat(document.getElementById("CantidadD").value);
-        let precio = parseFloat(document.getElementById("PrecioD").value);
-        valores[4].innerHTML = (cantidad * precio).toFixed(2);
-
+    // boton eliminar
+    let celdaEliminar = document.createElement("td");
+    let boton = document.createElement("button");
+    boton.innerHTML = '❌';
+    boton.addEventListener("click", function () {
+        fila.remove();
         calcularTotal();
-    };
+    });
+    celdaEliminar.appendChild(boton);
+    fila.appendChild(celdaEliminar);
 
+    // celda boton modificar
+    let celdaModificar = document.createElement("td");
+    let botonModificar = document.createElement("button");
+    botonModificar.innerHTML = '🔑';
+    botonModificar.addEventListener("click", function () {
+        // cargar valores
+        let celdas = fila.querySelectorAll("td");
+        document.getElementById("codigoD").value = celdas[0].textContent;
+        document.getElementById("descripcionD").value = celdas[1].textContent;
+        document.getElementById("cantidadD").value = celdas[2].textContent;
+        document.getElementById("precioD").value = celdas[3].textContent;
+
+        // configurar boton acutaliar
+        document.getElementById("modificar").onclick = function () {
+            celdas[0].textContent = document.getElementById("codigoD").value;
+            celdas[1].textContent = document.getElementById("descripcionD").value;
+            celdas[2].textContent = document.getElementById("cantidadD").value;
+            celdas[3].textContent = document.getElementById("precioD").value;
+
+            let cantidadMod = parseFloat(document.getElementById("cantidadD").value);
+            let precioMod = parseFloat(document.getElementById("precioD").value);
+            celdas[4].textContent = (cantidadMod * precioMod).toFixed(2);
+
+            calcularTotal();
+        };
+    });
+    celdaModificar.appendChild(botonModificar);
+    fila.appendChild(celdaModificar);
+    calcularTotal();
 }
