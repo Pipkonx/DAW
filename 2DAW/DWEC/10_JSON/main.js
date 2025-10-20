be1bc46bb3c43d71678fe74b450c1b6e57acd549
pipkon <pipkon.proyectos@gmail.com>
2025-10-20 11:42:40 +0200

    feat(DWEC/10_JSON): Mejora de la funcionalidad de visualización de datos JSON
    - Añadir nuevos estilos CSS para tablas y botones
    - Implementar la visualización de tablas para datos JSON con opciones de filtrado
    - Añadir un menú desplegable de selección de autor y el filtrado correspondiente
    - Crear funciones para mostrar publicaciones específicas por ID o rango
    - Mejorar la interfaz de usuario con botones con estilo y un diseño de tabla adaptable

--- 2DAW/DWEC/10_JSON/main.js
+++ 2DAW/DWEC/10_JSON/main.js
@@ -3,3 +3,102 @@ function cargaArticulos() {
         .then((response) => response.json())
         .then((json) => console.log(json));
 }
+
+function mostrarArticulos() {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+        .then((response) => response.json())
+        .then((data) => {
+            const table = document.querySelector("tbody");
+            const contenidoTabla = data;
+            for (let i = 0; i < contenidoTabla.length; i++) {
+                const fila = document.createElement("tr");
+                fila.innerHTML = `
+                <td>${contenidoTabla[i].id}</td>
+                <td>${contenidoTabla[i].title}</td>
+                <td>${contenidoTabla[i].body}</td>
+                `;
+                table.appendChild(fila);
+            }
+        })
+        .catch((error) => console.log(error));
+}
+
+function muestra() {
+    const id = document.querySelector("#texto").value;
+    // fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
+    fetch(`https://jsonplaceholder.typicode.com/posts/` + id)
+        .then((response) => response.json())
+        .then((data) => {
+            const table = document.querySelector("tbody");
+            const fila = document.createElement("tr");
+            fila.innerHTML = `
+            <td>${data.id}</td>
+            <td>${data.title}</td>
+            <td>${data.body}</td>
+            `;
+            table.appendChild(fila);
+            // table.innerHTML = fila.innerHTML;
+        })
+        .catch((error) => console.log(error));
+}
+
+function cargarAutor() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+        .then((response) => response.json())
+        .then((data) => {
+            const select = document.querySelector("#autor");
+            for (let i = 0; i < data.length; i++) {
+                const option = document.createElement("option");
+                // option.innerHTML = `
+                // <option value="${data[i].id}">${data[i].name}</option>
+                // `;
+                option.value = data[i].id;
+                option.textContent = data[i].name;
+                select.appendChild(option);
+
+            }
+        })
+        .catch((error) => console.error(error));
+}
+
+function mostrarArticulosHasta() {
+    const id = parseInt(document.querySelector("#hasta").value);
+    fetch('https://jsonplaceholder.typicode.com/posts')
+        .then((response) => response.json())
+        .then((data) => {
+            const table = document.querySelector("tbody");
+            //slice te permite sacar por pantalla la cantidadd que quieras
+            const contenidoTabla = data.slice(0, id);
+            for (let i = 0; i < contenidoTabla.length; i++) {
+                const fila = document.createElement("tr");
+                fila.innerHTML = `
+                <td>${contenidoTabla[i].id}</td>
+                <td>${contenidoTabla[i].title}</td>
+                <td>${contenidoTabla[i].body}</td>
+                `;
+                table.appendChild(fila);
+            }
+        })
+        .catch((error) => console.log(error));
+}
+
+function cargarArticulos() {
+    const autor = parseInt(document.querySelector("#autor").value);
+    fetch('https://jsonplaceholder.typicode.com/posts')
+        .then((response) => response.json())
+        .then((data) => {
+            const table = document.querySelector("tbody");
+            for (let i = 0; i < data.length; i++) {
+                if (autor == parseInt(data[i].userId)) {
+                    const fila = document.createElement("tr");
+                    fila.innerHTML = `
+                        <td>${data[i].id}</td>
+                        <td>${data[i].title}</td>
+                        <td>${data[i].body}</td>
+                        `;
+                    table.appendChild(fila);
+                }
+            }
+        })
+        .catch((error) => console.error(error));
+}
---
+++ 2DAW/DWES/htdocs/EjerciciosBasicos/T2/06_FEnvioFicheros/40_visorDeFotos/index.php
@@ -0,0 +1,47 @@
+<?php
+// Seleccionamos la foto
+if (!isset($_POST['img'])):
+?>
+    <!DOCTYPE html>
+    <html lang="es">
+
+    <head>
+        <meta charset="UTF-8">
+        <title>Visor de Fotos</title>
+    </head>
+
+    <body>
+        <h1>Selecciona una imagen</h1>
+        <form action="" method="post">
+            <label for="img">Sube un fichero: </label>
+            <input type="file" id="img" name="img" required>
+            <button type="submit">Subir y ver</button>
+        </form>
+    </body>
+
+    </html>
+    <?php
+// mostramos la foto
+else:
+    if (isset($_POST["img"])) :
+        $img = $_POST['img'];
+    ?>
+        <!DOCTYPE html>
+        <html lang="es">
+
+        <head>
+            <meta charset="UTF-8">
+            <title>Visor de Fotos</title>
+        </head>
+
+        <body>
+            <h1>Imagen seleccionada</h1>
+            <img src="<?= $img ?>" alt="No cargo la foto correctamente" style="max-width:100%;">
+            <br><br>
+            <a href="?">Elegir otra opcion</a>
+        </body>
+
+        </html>
+<?php
+    endif;
+endif;
