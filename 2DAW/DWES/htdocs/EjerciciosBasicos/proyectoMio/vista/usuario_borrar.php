0a2aa86242f5c5ddf5428232efef98be3d32d1dd
Pipkonx <pipkon.proyectos@gmail.com>
2025-10-28 18:26:57 +0100

    funcion borrar cambiada del operador ?? al ternario

--- 2DAW/DWES/htdocs/EjerciciosBasicos/proyectoMio/vista/usuario_borrar.php
+++ 2DAW/DWES/htdocs/EjerciciosBasicos/proyectoMio/vista/usuario_borrar.php
@@ -1,7 +1,8 @@
 <h2>¿Eliminar este usuario?</h2>
-<p>Nombre: <?= $datos['nombre'] ?></p>
-<p>Email: <?= $datos['email'] ?></p>
+<p>Nombre: <?= isset($datos['nombre']) ? $datos['nombre'] : '' ?></p>
+<p>Email: <?= isset($datos['email']) ? $datos['email'] : '' ?></p>
 <form method="POST">
+    <input type="hidden" name="id" value="<?= isset($datos['id']) ? $datos['id'] : '' ?>">
     <input type="submit" value="Confirmar borrado">
 </form>
 <a href="index.php">Cancelar</a>
