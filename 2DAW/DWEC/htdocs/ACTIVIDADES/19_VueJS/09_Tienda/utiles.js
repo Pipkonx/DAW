a018481dd6aea52ef516e01e2c55040d91711d89
Pipkon Dev <corderorafa0@gmail.com>
2026-01-21 10:54:14 +0100

    Delete desde la api

--- 2DAW/DWEC/htdocs/ACTIVIDADES/19_VueJS/09_Tienda/utiles.js
+++ 2DAW/DWEC/htdocs/ACTIVIDADES/19_VueJS/09_Tienda/utiles.js
@@ -13,15 +13,23 @@ var app = new Vue({
     },
     methods: {
         eliminar: function(id) {
-            // usamos filter para que devuleva todos menos el que tenga el id
+            //! usamos filter para que devuleva todos menos el que tenga el id
             // this.items = this.items.filter(item => item.id !==id);
-            // hacerlo sin filter seria con un for
-            for (let i = 0; i < this.items.length; i++) {
-                if (this.items[i].id == id) {
-                    //splice es para eliminar 
-                    this.items.splice(i, 1);
-                }
-            }
+            //! hacerlo sin filter seria con un for
+            // for (let i = 0; i < this.items.length; i++) {
+            //     if (this.items[i].id == id) {
+            //         //splice es para eliminar 
+            //         this.items.splice(i, 1);
+            //     }
+            // }
+
+            //! ahora eliminando de la api
+            fetch(`https://fakestoreapi.com/products/${id}`, {
+                method: 'DELETE',
+            })
+            .then(response => response.json())
+            .then(data => { console.log(data);
+            });
         }
     }
 });
